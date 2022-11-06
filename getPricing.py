da464d2199c2ea94f41f18eaad4fd2b8956ee955
Jeff <jeff@onegovee.com>
2022-11-06 12:50:29 -0500

    get data and format into json

--- getPricing.py
+++ getPricing.py
@@ -1,6 +1,21 @@
-import urllib.request
+# import urllib library
+from urllib.request import urlopen
+  
+# import json
+import json
+# store the URL in url as 
+# parameter for urlopen
+url = "https://www.expresslanes.com/maps-api/infra-price-confirmed-all"
+  
+# store the response of URL
+response = urlopen(url)
+  
+# storing the JSON response 
+# from url in data
+data = json.loads(response.read())
+  
+# print the json response
+# print(data_json)
 
-jsonUrl = 'https://www.expresslanes.com/maps-api/infra-price-confirmed-all'
-
-jsonData, headers = urllib.request.urlretrieve(jsonUrl, filename="data/pricing.json")
-urllib.request.urlcleanup()
+with open( "data/pricing.json" , "w" ) as write:
+    json.dump( data , write )
