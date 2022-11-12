91fc813685ffb19bc837436780047f476bc62030
Jeff <jeff@onegovee.com>
2022-11-12 17:19:31 -0500

    make json pretty

--- getPricing.py
+++ getPricing.py
@@ -1,21 +1,14 @@
-# import urllib library
-from urllib.request import urlopen
-  
-# import json
 import json
-# store the URL in url as 
-# parameter for urlopen
+from urllib.request import urlopen
+
 url = "https://www.expresslanes.com/maps-api/infra-price-confirmed-all"
   
-# store the response of URL
+# store URL response
 response = urlopen(url)
   
-# storing the JSON response 
-# from url in data
+# store JSON response from url in data
 data = json.loads(response.read())
   
-# print the json response
-# print(data_json)
-
+# store pretty JSON response in a file
 with open( "data/pricing.json" , "w" ) as write:
-    json.dump( data , write )
+    json.dump( data , write, indent=2 )
